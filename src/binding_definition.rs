fc67830a8ebee2bd8656c1436a650c3641f95455
cat <kittynekostudio@gmail.com>
2024-10-12 14:17:00 -0700

    Part 4: Backtracking. Added error handling.

--- src/binding_definition.rs
+++ src/binding_definition.rs
@@ -9,25 +9,25 @@ pub struct BindingDef {
 }
 
 impl BindingDef {
-    pub fn new(string: &str) -> (&str, Self) {
-        let string = utils::tag("let", string);
+    pub fn new(string: &str) -> Result<(&str, Self), String> {
+        let string = utils::tag("let", string)?;
+        let (string, _) = utils::extract_whitespace1(string)?;
+
+        let (string, name) = utils::extract_identifier(string)?;
         let (string, _) = utils::extract_whitespace(string);
 
-        let (string, name) = utils::extract_identifier(string);
+        let string = utils::tag("=", string)?;
         let (string, _) = utils::extract_whitespace(string);
 
-        let string = utils::tag("=", string);
-        let (string, _) = utils::extract_whitespace(string);
+        let (string, value) = Expression::new(string)?;
 
-        let (string, value) = Expression::new(string);
-
-        (
+        Ok((
             string,
             Self {
                 name: name.to_string(),
                 val: value,
             },
-        )
+        ))
     }
     pub fn evaluate(&self, environment: &mut Enviroment) {
         environment.store_binding(self.name.clone(), self.val.evaluate());
@@ -43,17 +43,24 @@ mod tests {
     fn parse_binding_def() {
         assert_eq!(
             BindingDef::new("let a = 10 / 2"),
-            (
+            Ok((
                 "",
                 BindingDef {
                     name: "a".to_string(),
-                    val: Expression {
+                    val: Expression::Operation {
                         lhs: Number(10),
                         rhs: Number(2),
                         op: Operation::Division
                     }
                 }
-            )
+            ))
+        );
+    }
+    #[test]
+    fn cannot_parse_binding_def_without_space_after_let() {
+        assert_eq!(
+            BindingDef::new("letaaa=1+2"),
+            Err("expected a space".to_string())
         );
     }
 }
